e4c04c03623d3a9b7a056da428ab0ac8111839ea
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-10 16:55:30 +0100

    Added create method to comment controller

--- server/controllers/comment.controller.js
+++ server/controllers/comment.controller.js
@@ -1,5 +1,20 @@
-import Comments from '../models/comment.model'
+import Comment from '../models/comment.model'
 import extend from 'lodash/extend'
 import errorHandler from './../helpers/dbErrorHandler'
 
+// Creates a new comemnt
+const create = async (req, res) => {
+    const comment = new Comment(req.body)
 
+    try {
+        await comment.save()
+
+        return res.status(200).json({
+            message: "Comment posted!"
+        })
+    } catch(err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}
