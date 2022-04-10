a3daa5ad1c6285dfd89cb87fe52f0101480b083b
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-10 21:30:27 +0100

    Added create function in api-comments.js

--- client/comments/api-comments.js
+++ client/comments/api-comments.js
@@ -9,8 +9,26 @@
       console.log(err)
     }
   }
+
+  const create = async (comment) => {
+    try {
+        let response = await fetch('/api/comments/', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(user)
+        })
+      return await response.json()
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
   
   export {
-    list
+    list,
+    create
   }
   
