283634ef1fa0d90b704b10621a4350efb8ff5a48
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-11 14:15:46 +0100

    Added function for deleting shop items in shop controller

--- server/controllers/shop.controller.js
+++ server/controllers/shop.controller.js
@@ -48,9 +48,24 @@ const read = (req, res) => {
     return res.json(req.product)
 }
 
+const remove = async(req, res) => {
+    try{
+        let product = req.product
+        let deletedProduct = await product.remove()
+        
+        res.json(deletedProduct)
+    } catch (err) {
+        console.log(err)
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}
+
 export default{
     create,
     list,
     productByID,
-    read
+    read,
+    remove
 }
