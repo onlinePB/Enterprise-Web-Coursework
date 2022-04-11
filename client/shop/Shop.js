093740700d9a7dab594523fdfc607f762e9ce238
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-11 17:04:31 +0100

    Bugfixing

--- client/shop/Shop.js
+++ client/shop/Shop.js
@@ -34,7 +34,7 @@ const useStyles = makeStyles(theme => ({
 export default function Shop() {
   const classes = useStyles()
   const [products, setProducts] = useState([])
-  const basket = []
+  var basket = []
 
   useEffect(() => {
     const abortController = new AbortController()
