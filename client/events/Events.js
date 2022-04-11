f2a04673b71fe6c7a660c67260872f0293b22110
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-12 00:52:58 +0100

    testing

--- client/events/Events.js
+++ client/events/Events.js
@@ -94,7 +94,7 @@ export default function Users() {
       } else {
         setValues({ ...values, error: '', open: true})
       }
-      document.location.reload() //Reload the page so they can see their comment
+      //document.location.reload() //Reload the page so they can see their comment
     })
     
   }
