a58ecf68978ac2d88e669013e34eaf82e40be2ab
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-11 14:14:40 +0100

    Added route for deleting shop items

--- server/routes/shop.routes.js
+++ server/routes/shop.routes.js
@@ -5,15 +5,19 @@ import shopCtrl from '../controllers/shop.controller'
 
 const router = express.Router()
 
+// Public routes
 router.route('/api/shop')
     .get(shopCtrl.list)
     
+router.route('/api/product/:itemID')
+    .get(shopCtrl.read)
 
+// Authenticated Routes
 router.route('/api/shop/:userID')
     .post(authCtrl.requireSignin, authCtrl.hasAdminAuthorization, shopCtrl.create)
 
-router.route('/api/product/:itemID')
-    .get(shopCtrl.read)
+router.route('/api/shop/:userID/:itemID')
+    .delete(authCtrl.requireSignin, authCtrl.hasAdminAuthorization, shopCtrl.remove)
 
 router.param('userID', userCtrl.userByID)
 router.param('itemID', shopCtrl.productByID)
