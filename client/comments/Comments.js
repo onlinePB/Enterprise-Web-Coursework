346be9d8796732aa49622aeed214d351aa503301
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-11 12:03:41 +0100

    Change backgroundColour from light to main

--- client/comments/Comments.js
+++ client/comments/Comments.js
@@ -27,7 +27,7 @@ const useStyles = makeStyles(theme => ({
   root: theme.mixins.gutters({
     padding: theme.spacing(1),
     margin: theme.spacing(5),
-    backgroundColor: theme.palette.primary.light
+    backgroundColor: theme.palette.primary.main
   }),
   title: {
     margin: `${theme.spacing(4)}px 0 ${theme.spacing(2)}px`,
