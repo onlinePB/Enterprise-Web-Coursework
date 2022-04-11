96adcb8ba14f92d381a8703e240beed925fd9b28
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-11 01:29:59 +0100

    Import missing module

--- client/comments/Comments.js
+++ client/comments/Comments.js
@@ -21,7 +21,7 @@ import Button from '@material-ui/core/Button';
 import auth from './../auth/auth-helper'
 import TextField from '@material-ui/core/TextField'
 import {create, remove} from './api-comments.js'
-
+import CardHeader from '@material-ui/core/CardHeader';
 
 const useStyles = makeStyles(theme => ({
   root: theme.mixins.gutters({
