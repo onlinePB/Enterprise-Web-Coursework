bd1c35c26262011e4d842a1a9d11714d163ed6e1
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-12 07:42:28 +0100

    Removed unneeded imports from MyEvents.js

--- client/events/MyEvents.js
+++ client/events/MyEvents.js
@@ -4,26 +4,13 @@ import Paper from '@material-ui/core/Paper'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListItemText from '@material-ui/core/ListItemText'
 import Avatar from '@material-ui/core/Avatar'
-import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
-import ArrowForward from '@material-ui/icons/ArrowForward'
-import Person from '@material-ui/icons/Person'
-import EventIcon from '@material-ui/icons/Event';
 import EventAvailableIcon from '@material-ui/icons/EventAvailable';
 import {Link} from 'react-router-dom'
 import {list} from './api-events'
-import Button from '@material-ui/core/Button'
 import auth from './../auth/auth-helper'
-import {read} from './../user/api-user'
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import {create} from './api-events'
-import TextField from '@material-ui/core/TextField'
 
 const useStyles = makeStyles(theme => ({
   root: theme.mixins.gutters({
