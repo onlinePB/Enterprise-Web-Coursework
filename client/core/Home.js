13d7e1ff40a98e56925e14b1b829425bdc8bf5e6
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-12 08:44:47 +0100

    Fix file extension on image import

--- client/core/Home.js
+++ client/core/Home.js
@@ -5,7 +5,7 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
-import myImg from './../assets/images/animalpicture.jpg'
+import myImg from './../assets/images/animalpicture.jpeg'
 import {Link} from 'react-router-dom'
 import {joke} from '../thirdparty/api-dadjokes.js'
 
