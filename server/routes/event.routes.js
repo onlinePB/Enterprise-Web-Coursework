3656d01dd6c62466f17590b473689d136d03e7ed
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-12 07:20:44 +0100

    Updated comments in event routes

--- server/routes/event.routes.js
+++ server/routes/event.routes.js
@@ -6,16 +6,21 @@ import userCtrl from '../controllers/user.controller'
 const router = express.Router()
 
 // Public routes
+// GET: Lists all events
 router.route('/api/events')
     .get(eventCtrl.list)
 
+// GET: Gets a particular event using it's ID
 router.route('/api/event/:eventID')
     .get(eventCtrl.read)
 
 // Authenticated routes
+// POST: Creates a new event
 router.route('/api/events/:userID')
     .post(authCtrl.requireSignin, authCtrl.hasAdminAuthorization, eventCtrl.create)
 
+// PUT: Updates an existing event
+// DELETE: Deletes an existing event
 router.route('/api/event/:userID/:eventID')
     .put(authCtrl.requireSignin, authCtrl.hasAuthorization, eventCtrl.update)
     .delete(authCtrl.requireSignin, authCtrl.hasAdminAuthorization, eventCtrl.remove)
