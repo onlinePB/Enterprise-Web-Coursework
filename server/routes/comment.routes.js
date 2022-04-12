b834cebbc86bfe3b7b18c0851d5cb7abafd9a11d
Ryan Wilson <47390291+onlinePB@users.noreply.github.com>
2022-04-12 07:19:30 +0100

    Updated comments in comment routes

--- server/routes/comment.routes.js
+++ server/routes/comment.routes.js
@@ -5,16 +5,22 @@ import userCtrl from '../controllers/user.controller'
 
 const router = express.Router()
 
+// GET: Gets all comments
+// POST: Creates a new comment
 router.route('/api/comments')
     .get(commentCtrl.list)
     .post(authCtrl.requireSignin, commentCtrl.create)
 
+// GET: Gets a particular comment using it's ID
 router.route('/api/comments/:commentID')
     .get(commentCtrl.read)
 
+// GET: Gets all replies to a comment using the original comment's ID
 router.route('/api/replies/:commentID')
     .get(commentCtrl.listReplies)
 
+// DELETE: Deletes a comment from the database
+// GET: Gets a particular comment using it's ID
 router.route('/api/comment/:userID/:commentID')
     .get(authCtrl.requireSignin, authCtrl.hasAuthorization, commentCtrl.read)
     .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, commentCtrl.remove)
